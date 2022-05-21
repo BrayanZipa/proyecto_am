a9fed90b336fab3994c352ad66da427ca81fb990
DAVIDBOTERO24 <99380676+DAVIDBOTERO24@users.noreply.github.com>
2022-05-21 02:12:54 -0500

    El commit 2

--- src/app/Servicio/crud.service.ts
+++ src/app/Servicio/crud.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Admin} from './Admin';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
-
+API: string='http://localhost/AdminPF'
   constructor() { }
 }
